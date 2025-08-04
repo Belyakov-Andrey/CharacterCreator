599bcd35d319a03955864f8c2a1e63279b652c39
Andrey <anbelyakov02@yandex.ru>
2025-08-04 21:02:19 +0300

    mini fix

---
+++ CharacterClasses/Mage.cs
@@ -0,0 +1,14 @@
+﻿namespace CharacterClasses
+{
+    public class Mage : Character
+    {
+        public override int Health => 60 + Endurance * 6;
+        public override int Damage => 5 + Intelligence * 3;
+        public override double CritChance => 0.15 + Intelligence * 0.015;
+
+        public override string GetClassDescription()
+        {
+            return "Маг - мощный заклинатель, чья сила зависит от интеллекта.";
+        }
+    }
+}
